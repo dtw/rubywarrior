b03a5e9d529efe53bad780119ab5eb6e8e8d1b6a
Phil Thiselton <dibblethewrecker:gmail.com>
2014-02-16 12:58:18 +0000

    Level 3

--- player.rb
+++ player.rb
@@ -1,10 +1,16 @@
 class Player
   def play_turn(warrior)
     # add your code here
-    if warrior.feel.empty? then
-    	warrior.walk!
-    elsif warrior.feel.enemy? then
-	warrior.attack!
-    end
+	  if warrior.feel.empty? then
+    	unless warrior.health < 20 then
+				warrior.walk!
+			else
+				warrior.rest!
+			end
+		else
+	    if warrior.feel.enemy? then
+				warrior.attack!
+ 	    end
+		end
   end
 end
