3aa0af96c3f1e60b93c226fe3d805d04887a413a
Phil T <dibblethewrecker@gmail.com>
2014-03-01 10:01:44 +0000

    Whitespace

--- player.rb
+++ player.rb
@@ -1,21 +1,21 @@
 class Player
   def play_turn(warrior)
     # add your code here
-	  if warrior.feel.empty? then
-    	unless warrior.health < 20 then
-				warrior.walk!
-			else
-    		if warrior.health < @health then
-					warrior.walk!
-				else
-					warrior.rest!
-				end
-			end
-		else
-	    if warrior.feel.enemy? then
-				warrior.attack!
- 	    end
-		end
-		@health = warrior.health
+    if warrior.feel.empty? then
+      unless warrior.health < 20 then
+        warrior.walk!
+      else
+        if warrior.health < @health then
+          warrior.walk!
+        else
+          warrior.rest!
+        end
+      end
+    else
+      if warrior.feel.enemy? then
+        warrior.attack!
+      end
+    end
+    @health = warrior.health
   end
 end
