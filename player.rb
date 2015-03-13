dfda109f6825a70d5e8c10c71c4a2928b8f5305f
Phil T. <dibblethewrecker@gmail.com>
2015-03-13 12:50:18 +0000

    Add some comments

--- player.rb
+++ player.rb
@@ -1,17 +1,21 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+    # is there a space in front?
     if warrior.feel.empty? then
+      # am I hurt AND not taking damage?
       if warrior.health < 20 and warrior.health >= @health then
         warrior.rest!
       else
         warrior.walk!
       end
+    # is there a captive in front?
     elsif warrior.feel.captive? then
       warrior.rescue!
+    # is there any enemy in front?
     elsif warrior.feel.enemy? then
       warrior.attack!
     end
+    # store current health
     @health = warrior.health
   end
 end
